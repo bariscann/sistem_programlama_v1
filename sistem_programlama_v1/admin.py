2c45f46eef4de81b694e6f4e8a3c7d32a1abae87
TopalSolcan <bc.brscn@gmail.com>
2017-04-20 20:33:56 +0300

    models created

--- sistem_programlama_v1/admin.py
+++ sistem_programlama_v1/admin.py
@@ -2,9 +2,15 @@ from django.contrib import admin
 from .models import Faculty
 from .models import Department
 from .models import Lecturer
+from .models import BasicLecture
+from .models import Principle
+from .models import Vote
 
 admin.site.register(Faculty)
 admin.site.register(Department)
 admin.site.register(Lecturer)
+admin.site.register(BasicLecture)
+admin.site.register(Principle)
+admin.site.register(Vote)
 
 # Register your models here.
--- sistem_programlama_v1/models.py
+++ sistem_programlama_v1/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinLengthValidator, MinValueValidator, MaxValueValidator
 from django.db import models
 
 
@@ -5,20 +6,40 @@ from django.db import models
 
 class Faculty(models.Model):
     # TODO:burada iki primary olmuş oldu normalde uygun fakat bence birini seçmemiz gerekiyor ne olacak
-    #id = models.AutoField(primary_key=True)
-    faculty_code = models.CharField(max_length=3, primary_key=True)
-    name = models.TextField()
+    id = models.AutoField(primary_key=True)
+    faculty_code = models.CharField(max_length=3, validators=[MinLengthValidator(3)], unique=True)
+    name = models.CharField(max_length=255)
 
 
 class Department(models.Model):
-    department_code = models.CharField(max_length=3, primary_key=True)
-    name=models.TextField()
+    id = models.AutoField(primary_key=True)
+    department_code = models.CharField(max_length=3, validators=[MinLengthValidator(3)], unique=True)
+    name=models.CharField(max_length=255)
     faculty = models.ForeignKey('Faculty')
 
 
 
 class Lecturer(models.Model):
-    lecturer_code = models.TextField(primary_key=True)
-    first_name = models.TextField()
-    last_name = models.TextField()
+    id = models.AutoField(primary_key=True)
+    lecturer_code = models.CharField(max_length=10, unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    department = models.ForeignKey('Department')
 
+class BasicLecture(models.Model):
+    id = models.AutoField(primary_key=True)
+    courseCode =  models.CharField(max_length=7, validators=[MinLengthValidator(7)], unique=True)
+    courseName = models.CharField(max_length=255)
+
+
+class Principle(models.Model):
+    id = models.AutoField(primary_key=True)
+    content = models.TextField()
+    importance = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(10)])
+    basic_lecture = models.ForeignKey('BasicLecture')
+
+class Vote(models.Model):
+    id = models.AutoField(primary_key=True)
+    value = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(10)])
+    lecture = models.ForeignKey('Lecture')
+    student = models.ForeignKey('Student')
